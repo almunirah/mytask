7cd488d7bf10db6e6ee1efa012213f6675c9d9d2
almunirah <almunirah.alam@gmail.com>
2026-08-10 10:39:33 +0800

    Add application layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'MyTask',description:'Personal Project & To-Do Management'}
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
